7c18ff7684c034a1b4ba426cf9d771c4fd47dee5
chaima.ghaddab <chaima.ghaddab@tum.de>
2022-09-19 10:20:54 +0200

    Comments for child's subsystem

--- VeggieTracker/Shared/ChildSubsystem/EditChild.swift
+++ VeggieTracker/Shared/ChildSubsystem/EditChild.swift
@@ -9,10 +9,12 @@ import SwiftUI
 import CoreLocation
 import Combine
 
+/// The view responsible for editing a Child's profile within the use's account
 struct EditChild: View {
     @Environment(\.presentationMode) private var presentationMode
     @ObservedObject private var viewModel: EditChildViewModel
     
+    /// Depending on whether the child exists already or is being created, the title of the sheet changes
     var navigationTitle: String {
         viewModel.id == nil ? "Create Child" : "Edit Child"
     }
@@ -24,14 +26,17 @@ struct EditChild: View {
     var body: some View {
         NavigationView {
             self.form
+                /// fills the entries with the saved information if the child exists
                 .onAppear(perform: viewModel.updateStates)
                 .navigationBarTitle(navigationTitle, displayMode: .inline)
                 .navigationBarBackButtonHidden(true)
                 .navigationBarItems(
+                    /// Button to cancel the editing/creation of the child's profile
                     leading:
                         Button(action : { self.presentationMode.wrappedValue.dismiss() }){
                             Text("Cancel").foregroundColor(Color.red)
                         },
+                    /// Button to save the editing/creation of the child's profile
                     trailing:
                         Button(action : {
                             viewModel.save()
@@ -43,12 +48,14 @@ struct EditChild: View {
         }.navigationViewStyle(StackNavigationViewStyle())
     }
     
+    ///  The form to fill to edit/create a child's profile
     private var form: some View {
         Form {
+            /// Entry for child's name
             Section(header: Text("Name")) {
                 TextField("Name", text: $viewModel.name)
             }
-            
+            /// Entry for child's age (Picker style)
             Section(header: Text("Age")) {
                 Picker("Age", selection: $viewModel.age) {
                     ForEach(1 ..< 19) {
