45b80d499de9a296f50473ce86bc558a20b155b5
chaima.ghaddab <chaima.ghaddab@tum.de>
2022-09-19 09:23:19 +0200

    notifications allowed

--- VeggieTracker/Shared/VeggieTrackerApp.swift
+++ VeggieTracker/Shared/VeggieTrackerApp.swift
@@ -6,12 +6,56 @@
 //
 
 import SwiftUI
+import UserNotifications
 
+class VeggieAppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+      registerForPushNotifications()
+      return true
+    }
+
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+          .requestAuthorization(
+            options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+            print("Permission granted: \(granted)")
+            guard granted else { return }
+            self?.getNotificationSettings()
+          }
+    }
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            print("Notification settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    func application(
+      _ application: UIApplication,
+      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+      let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+      let token = tokenParts.joined()
+      print("Device Token: \(token)")
+    }
+    func application(
+      _ application: UIApplication,
+      didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
+      print("Failed to register: \(error)")
+    }
+
+}
 
 @main
 struct VeggieTrackerApp: App {
+    @UIApplicationDelegateAdaptor(VeggieAppDelegate.self) var appDelegate
     @StateObject var model: VeggieTrackerModel = MockModel()
-    
     var body: some Scene {
         WindowGroup {
             ContentView().environmentObject(model)
