25c5c84b74033465c50b6bf9390aba3bd12fa576
chaima.ghaddab <chaima.ghaddab@tum.de>
2022-09-26 14:11:00 +0200

    MockModel instead of Model

--- VeggieTracker/Shared/VeggieTrackerApp.swift
+++ VeggieTracker/Shared/VeggieTrackerApp.swift
@@ -11,6 +11,7 @@ import UserNotifications
 @main
 struct VeggieTrackerApp: App {
     @StateObject var model: VeggieTrackerModel = MockModel()
+//    VeggieTrackerModel(user: User(username: "Nathalie"), children: [Child](), meals: [Meal](), notifications: [Notification]())
     var body: some Scene {
         WindowGroup {
             ContentView().environmentObject(model)
